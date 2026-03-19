8a78562e0c50bc2c344271c5f6f18a9e16e3938d
Warren <42559252+wsmarshall@users.noreply.github.com>
2026-03-19 12:15:29 -0700

    reinitializes codebase from the ground up

--- graphQL-node-ts/src/index.ts
+++ graphQL-node-ts/src/index.ts
@@ -1,16 +1,1 @@
-import "graphql-import-node";
-import { execute, parse } from "graphql";
-import { schema } from "./schema.ts";
-
-async function main() {
-    const myQuery = parse(`query { info }`);
-
-    const result = await execute({
-        schema, 
-        document: myQuery,
-    });
-
-    console.log(result);
-}
-
-main();
+console.log('Hello, World!');
