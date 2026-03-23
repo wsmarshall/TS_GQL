f1cf3bbe9dfd30535d836905c464045511d6d147
Warren <42559252+wsmarshall@users.noreply.github.com>
2026-03-23 15:09:17 -0700

    starts updating resolvers for Link

--- graphQL-node-ts/src/schema.ts
+++ graphQL-node-ts/src/schema.ts
@@ -1,11 +1,32 @@
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import typeDefs from "./schema.graphql";
 
+type Link = {
+  id: string;
+  url: string;
+  description: string;
+}
+
+const links: Link[] = [{
+  id: 'link-0',
+  url: 'www.old.shmeddit.com',
+  description: 'The Back Page of the Internet'
+}]
+
 const resolvers = {
   Query: {
-    info: () => 'Test',
+    info: () => 'This is the API for Shmeddit',
+    feed: () => links,
+  },
+  Link: {
+  id: (parent: Link) => parent.id,
+  description: (parent: Link) => parent.description,
+  url: (parent: Link) => parent.url,
   }
 }
+
+
+
 
 export const schema = makeExecutableSchema({
   typeDefs,
