7d565c052a84f7fe632d3719abf0f060d1af124d
Warren <42559252+wsmarshall@users.noreply.github.com>
2026-04-10 15:35:33 -0700

    adds Mutation to resolvers

--- graphQL-node-ts/src/schema.ts
+++ graphQL-node-ts/src/schema.ts
@@ -22,8 +22,23 @@ const resolvers = {
   id: (parent: Link) => parent.id,
   description: (parent: Link) => parent.description,
   url: (parent: Link) => parent.url,
-  }
-}
+  },
+  Mutation: {
+    post: (parent: unknown, args: { description: string, url: string}) => {
+      let idCount = links.length;
+
+      const link: Link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+
+      links.push(link);
+
+      return link;
+    },
+  },
+};
 
 
 
