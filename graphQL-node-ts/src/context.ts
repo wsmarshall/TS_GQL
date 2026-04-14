c783d77ca9dda91dec4a336732998c7e39649a7c
Warren <42559252+wsmarshall@users.noreply.github.com>
2026-04-14 16:28:07 -0700

    makes prisma object available during execution of resolvers

--- graphQL-node-ts/src/context.ts
+++ graphQL-node-ts/src/context.ts
@@ -1,3 +1,20 @@
+import { PrismaClient } from "../prisma/generated/prisma/client.ts"
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+
+const prisma = new PrismaClient({
+    adapter: new PrismaBetterSqlite3(
+        {
+          url: process.env.DATABASE_URL || "file:./dev.db",
+        },
+    ),
+});
+
+export type GraphQLContext = {
+    prisma: PrismaClient;
+};
+
 export async function contextFactory() {
-    return {};
+    return {
+        prisma,
+    };
 }
